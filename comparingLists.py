8a75e119ee4f5a18d7ce8de83e74599709ca86f3
tycoon87 <tyson.woodruff@outlook.com>
2017-09-18 12:45:26 -0700

    tryed to make conditions a function but wont take two peramaders

--- comparingLists.py
+++ comparingLists.py
@@ -1,8 +1,43 @@
-l1 = ['celery','carrots','bread','milk']
-l2 = ['celery','carrots','bread','cream']
+#l1 = ['celery','carrots','bread','milk']
+#l2 = ['celery','carrots','bread','cream']
+#
+#def compare(arr):
+#    for i in l1:
+#        for j in l2:
+#            if i == j:
+#                print "true"
+#            
+#compare(l1, l2)
+#this works in vito once i call it a function it will not take 2 peramaders....
+
+#*****************************************************
+#define function
+def compare_lists(list_one, list_two):
+#    if coditions list one is the same as list two
+    if list_one == list_two:
+#        print the folowing
+        print "The lists are the same"
+#        outherwise print this instead
+    else:
+        print "The lists are not the same"
+
+list_one = [1,2,5,6,2]
+list_two = [1,2,5,6,2]
+
+compare_lists(list_one, list_two)
+
+list_one = [1,2,5,6,5]
+list_two = [1,2,5,6,5,3]
+
+compare_lists(list_one, list_two)
+
+list_one = [1,2,5,6,5,16]
+list_two = [1,2,5,6,5]
 
 
-for i in l1:
-    for j in l2:
-        if i == j:
-            print "true"
+compare_lists(list_one, list_two)
+
+list_one = ['celery','carrots','bread','milk']
+list_two = ['celery','carrots','bread','cream']
+
+compare_lists(list_one, list_two)
